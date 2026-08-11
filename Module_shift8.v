db821b4403703e671a0224e24beefd40b03bf749
Vivek Gattu <gattuvivek03@gmail.com>
2026-08-11 20:23:45 +0530

    Add top_module with 8-bit D flip-flops
    You are given a module my_dff8 with two inputs and one output (that implements a set of 8 D flip-flops). Instantiate three of them, then chain them together to make a 8-bit wide shift register of length 3. In addition, create a 4-to-1 multiplexer (not provided) that chooses what to output depending on sel[1:0]: The value at the input d, after the first, after the second, or after the third D flip-flop

---
+++ Module_shift8.v
@@ -0,0 +1,21 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    wire [7:0]w1;
+    my_dff8 M1(clk,d,w1);
+    wire [7:0]w2;
+    my_dff8 M2(clk,w1,w2);
+    wire [7:0]w3;
+    my_dff8 M3(clk,w2,w3);
+    always @(*)
+        case(sel)
+            2'b00: q=d;
+            2'b01: q=w1;
+            2'b10: q=w2;
+            2'b11: q=w3;
+        endcase
+    
+endmodule
